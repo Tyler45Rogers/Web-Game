acdb21f3ef4943b4ccdcd4565ec8ae1f5cfb538f
Tyler Rogers <tyler45rogers@gmail.com>
2026-06-07 21:36:35 -0500

    Adding name and description to site, still need links to social media, need to improve the project cards (those buttons are super ugly :( )

--- webApp/src/routes/+page.svelte
+++ webApp/src/routes/+page.svelte
@@ -1,14 +1,28 @@
 <script>
+    import MoveDown from '@lucide/svelte/icons/move-down';
     import ProjectCard from '$lib/components/ProjectCard.svelte';
     import { projects } from '$lib/projects/projects.js';
 </script>
 
-<section>
 
-    <div class="flex flex-col justify-items-center mx-auto">
-        <p class="text-5xl font-bold mt-10 text-center"> Projects </p>
+<!--Who Am I-->
+<section class="mt-10">
+<div class="flex flex-col mx-auto gap-10">
+    <p class="text-9xl font-bold text-left justify-start animate-pulse bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent pb-8"> Tyler Rogers </p>
+    <div class="flex flex-row justify-end">
+        <p class="text-2xl font-bold mt-10 mr-10 max-w-md text-right"> I am a software engineer from Iowa with a foundation in web development, databases, multiple programming languages, and more. Below are some of my projects that I have made. </p>
+    </div>
+    <div class ="flex justify-center">
+        <MoveDown size={48} class="flex justify-center"/>
+    </div>
     
 
+</div>
+
+<!--Projects-->
+<p class="text-9xl font-bold text-left justify-start animate-pulse bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent pb-8"> Projects </p>
+
+    <div class="flex flex-col justify-items-center mx-auto">
         <div class = "grid md:grid-cols-3 sm:grid-cols-1 justify-items-center mx-auto mb-10 mt-10 gap-10">
             {#each projects as project}
             <ProjectCard
