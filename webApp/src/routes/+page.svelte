5b461d98548c91df8d8a0b4bd1e02bc65b1e7c65
Tyler Rogers <tyler45rogers@gmail.com>
2026-06-08 17:11:18 -0500

    Better styling for smaller screens, still need links to other pages, and better project cards

--- webApp/src/routes/+page.svelte
+++ webApp/src/routes/+page.svelte
@@ -8,19 +8,20 @@
 <!--Who Am I-->
 <section class="mt-10">
 <div class="flex flex-col mx-auto gap-10">
-    <p class="text-9xl font-bold text-left justify-start animate-pulse bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent pb-8"> Tyler Rogers </p>
+    <p class="text-5xl md:text-9xl font-bold text-left justify-start animate-pulse bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent pb-8"> Tyler Rogers </p>
     <div class="flex flex-row justify-end">
-        <p class="text-2xl font-bold mt-10 mr-10 max-w-md text-right"> I am a software engineer from Iowa with a foundation in web development, databases, multiple programming languages, and more. Below are some of my projects that I have made. </p>
+        <p class="text-lg md:text-2xl font-bold mt-10 mr-10 max-w-md text-right"> I am a software engineer from Iowa with a foundation in web development, databases, multiple programming languages, and more. Below are some of my projects that I have made. </p>
     </div>
     <div class ="flex justify-center">
-        <MoveDown size={48} class="flex justify-center"/>
+        <MoveDown size={48} class="text-violet-500 animate-bounce md:hidden" />
+        <MoveDown size={96} class="text-violet-500 animate-bounce hidden md:block" />
     </div>
     
 
 </div>
 
 <!--Projects-->
-<p class="text-9xl font-bold text-left justify-start animate-pulse bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent pb-8"> Projects </p>
+<p class="text-5xl md:text-9xl font-bold text-left justify-start animate-pulse bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent pb-8"> Projects </p>
 
     <div class="flex flex-col justify-items-center mx-auto">
         <div class = "grid md:grid-cols-3 sm:grid-cols-1 justify-items-center mx-auto mb-10 mt-10 gap-10">
