baf4e6233962445ad85b6a90afb7813997dbf2d7
Tyler Rogers <tyler45rogers@gmail.com>
2026-06-27 15:02:37 -0500

    better way to write the colors for modal

--- webApp/src/lib/components/ProjectModal.svelte
+++ webApp/src/lib/components/ProjectModal.svelte
@@ -8,9 +8,6 @@
     function handleKeydown(e) {
         if (e.key === 'Escape') onclose();
     }
-
-    //Get the theme for background - nord or dark
-    const activeTheme = document.documentElement.getAttribute('data-theme');
 </script>
 
 <dialog
@@ -20,48 +17,19 @@
     onkeydown={handleKeydown}
     aria-label={title}
 >
-    {#if (activeTheme == 'dark')}
-        <div class="bg-zinc-800 rounded-xl shadow-xl max-w-2xl w-full max-h-[90vh] overflow-y-auto">
-            <div class="flex items-center justify-between p-6 border-b border-gray-200">
-                <h2 class="text-2xl font-bold">{title}</h2>
-                <button
-                    onclick={onclose}
-                    class="hover: text-3xl leading-none"
-                    aria-label="Close modal"
-                >
-                    &times;
-                </button>
-            </div>
+    <div class="bg-secondary [[data-theme=dark]_&]:bg-zinc-800 rounded-xl shadow-xl max-w-2xl w-full max-h-[90vh] overflow-y-auto">
 
-            {#if screenshot}
-                <img src={screenshot} alt="{title} screenshot" class="w-full object-cover" />
-            {/if}
-
-            <div class="p-6">
-                <p class="leading-relaxed">{details}</p>
-            </div>
-        </div>  
-    {:else}
-          <div class="bg-secondary rounded-xl shadow-xl max-w-2xl w-full max-h-[90vh] overflow-y-auto">
-            <div class="flex items-center justify-between p-6 border-b border-gray-200">
-                <h2 class="text-2xl font-bold">{title}</h2>
-                <button
-                    onclick={onclose}
-                    class="hover: text-3xl leading-none"
-                    aria-label="Close modal"
-                >
-                    &times;
-                </button>
-            </div>
-
-            {#if screenshot}
-                <img src={screenshot} alt="{title} screenshot" class="w-full object-cover" />
-            {/if}
-
-            <div class="p-6">
-                <p class="leading-relaxed">{details}</p>
-            </div>
-        </div> 
-    {/if}
-
+        <div class="flex items-center justify-between p-6 border-b border-gray-200">
+            <h2 class="text-2xl font-bold">{title}</h2>
+            <button onclick={onclose} class="text-3xl leading-none" aria-label="Close modal">
+                &times;
+            </button>
+        </div>
+        {#if screenshot}
+            <img src={screenshot} alt="{title} screenshot" class="w-full object-cover" />
+        {/if}
+        <div class="p-6">
+            <p class="leading-relaxed">{details}</p>
+        </div>
+    </div>
 </dialog>
